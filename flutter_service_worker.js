ee102334e27845ef84a7141b6b44611c016379e5
Sony Tran <sonytran@gmail.com>
2023-01-25 10:01:04 -0800

    Hyperlink Test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,10 +24,10 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "44bf6e1456cf73d382e674fa4dec6867",
-"/": "44bf6e1456cf73d382e674fa4dec6867",
-"main.dart.js": "ba55a345a5b2c132d7951ce93d14e36b",
-"manifest.json": "e2d472282ed7256aeee58cdb8be2274c",
+"index.html": "3605a4e67274adc02d08353083e2cfa6",
+"/": "3605a4e67274adc02d08353083e2cfa6",
+"main.dart.js": "3b59e88f7af0bd77d86d3149c72664fa",
+"manifest.json": "13c8e6d04bbc46b0438f5e7786396a4d",
 "version.json": "c8636e48c7828e3a2205de291cf36e6c",
 "worker.js": "f916974921b8b3d98f9fdfc4b68e5432"
 };
